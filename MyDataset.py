331e60349b998cd2a6eddf605cba92163f6081aa
Osilly <m18010704827@163.com>
2021-11-04 11:01:02 +0800

    update the part of dataset

--- MyDataset.py
+++ MyDataset.py
@@ -7,15 +7,15 @@
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
-from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import maxabs_scale
 import os
 
 
 # In[2]:
 
 
-class GetTraindata:
-    def __init__(self, path):
+class GetData:
+    def __init__(self, path, signal_size = 4096):
         real_classes = [d for d in os.listdir(os.path.join(path, '真实信号')) if os.path.isfile(os.path.join(path, '真实信号',d))]
         simulation_classes = [d for d in os.listdir(os.path.join(path, '模拟信号')) if os.path.isfile(os.path.join(path, '模拟信号',d))]
         real_classes.sort(key = lambda x:int(x[0:-4]))
@@ -28,27 +28,32 @@ class GetTraindata:
             simulation_signal.append(np.loadtxt(os.path.join(path, '模拟信号', file_path)))
             
         real_signal = np.array(real_signal)
-        simulation_signal = np.array(simulation_signal) * 1000
-        simulation_signal = np.pad(simulation_signal,((0,0),(0,4096 - simulation_signal.shape[-1])),'constant',constant_values = (0,0))
+        simulation_signal = np.array(simulation_signal)
+        simulation_signal = np.pad(simulation_signal,((0,0),(0,signal_size - simulation_signal.shape[-1])),'constant',constant_values = (0,0))
         
-        self.max_abs_scaler = MaxAbsScaler()
-        data = np.concatenate((real_signal,simulation_signal),axis=0)
-        self.max_abs_scaler.fit_transform(data)
+#         self.max_abs_scaler = MaxAbsScaler(axis=1)
+#         data = np.concatenate((real_signal,simulation_signal),axis=0)
+#         self.max_abs_scaler.fit_transform(data)
                
-        real_signal = self.max_abs_scaler.transform(real_signal)
-        simulation_signal = self.max_abs_scaler.transform(simulation_signal)        
+#         real_signal = self.max_abs_scaler.transform(real_signal)
+#         simulation_signal = self.max_abs_scaler.transform(simulation_signal)        
+        
+        real_signal = maxabs_scale(real_signal, axis=1)
+        simulation_signal = maxabs_scale(simulation_signal, axis=1) 
         self.real_signal = real_signal[:,np.newaxis,:]
+#         print(self.real_signal)
         self.simulation_signal = simulation_signal[:,np.newaxis,:]
+#         print(self.simulation_signal)
    
-    def get_scaler(self):       
-        return self.max_abs_scaler   
+#     def get_scaler(self):       
+#         return self.max_abs_scaler   
     
     def get_data(self):
         return self.real_signal, self.simulation_signal
     
-    def get_origin_data(self, data): # input_size:[*,4096],output_size:[*,4096]
-        origin_data = self.max_abs_scaler(data)
-        return origin_data
+#     def get_origin_data(self, data): # input_size:[*,4096],output_size:[*,4096]
+#         origin_data = self.max_abs_scaler(data)
+#         return origin_data
 
 
 # In[3]:
